bc113518d767c6f8bdab50ebcf008c5255819f08
Jelani Woods <jelani@firstdraft.com>
2020-07-07 18:23:54 -0500

    Wip

--- darts.rb
+++ darts.rb
@@ -2,14 +2,18 @@
 
 # Darts is a game where players throw darts to a target.
 
-# In our particular instance of the game, the target rewards with 4 different amounts of points, depending on where the dart lands.
+# In our particular instance of the game, the target rewards with 4 different amounts of points,
+#  depending on where the dart lands.
 
 # If the dart lands:
 #  outside the target: 0 points.
 #  in the outer circle of the target: 1 point.
 #  in the middle circle of the target: 5 points.
 #  in the inner circle of the target: 10 points.
-#  The outer circle has a radius of 10 units (This is equivalent to the total radius for the entire target), the middle circle a radius of 5 units, and the inner circle a radius of 1. They are all centered to the same point (That is, the circles are concentric) defined by the coordinates (0, 0).
+#  The outer circle has a radius of 10 units
+#    (This is equivalent to the total radius for the entire target),
+#    the middle circle a radius of 5 units, and the inner circle a radius of 1.
+#    They are all centered to the same point (That is, the circles are concentric) defined by the coordinates (0, 0).
 
 # Write a program that asks for a point in the target
 #  (defined by its real Cartesian coordinates x and y),
