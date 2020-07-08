f35c9619d3ee74b9776d7ab6155c93d8ca0d25d1
Jelani Woods <jelani@firstdraft.com>
2020-07-07 19:56:15 -0500

    Update secret_encoder_spec.rb

--- spec/scripts/secret_encoder_spec.rb
+++ spec/scripts/secret_encoder_spec.rb
@@ -6,13 +6,13 @@ describe "secret_encoder.rb" do
 end
 
 describe "secret_encoder.rb" do
-  it "should print 'D4n't t2ll 1ny4n2 45r c4d2' when the input is 'don't tell anyone our code'", points: 1 do
+  it "should print 'D4n't t2ll 1ny4n2 45r c4d2' when the input is 'Don't tell anyone our code'", points: 1 do
 
  # Un-require secret_encoder.rb
     secret_encoder = $".select{|r| r.include? 'secret_encoder.rb'}
     $".delete(secret_encoder.first)
 
-    allow_any_instance_of(Object).to receive(:gets).and_return("don't tell anyone our code\n")
+    allow_any_instance_of(Object).to receive(:gets).and_return("Don't tell anyone our code\n")
     expect { require_relative '../../secret_encoder.rb' }.to output(/D4n't t2ll 1ny4n2 45r c4d2/).to_stdout
   end
 end
